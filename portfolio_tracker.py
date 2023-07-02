58e871440e2ca826f875444f5a9c4c29858038b7
Caolan Rafferty <caolanraff@hotmail.co.uk>
2023-07-02 11:35:10 +0800

    first commit

---
+++ portfolio_tracker.py
@@ -0,0 +1,347 @@
+import pandas as pd
+import yfinance as yf
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import argparse
+from yahoo_fin import stock_info as si
+from matplotlib.backends.backend_pdf import PdfPages
+import pdfrw
+import os
+from fpdf import FPDF
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--timeframe', type=str, help='timeframe [MTD|YTD|adhoc]')
+parser.add_argument('--start', default='', type=str, help='start date [YYYY-MM-DD]')
+parser.add_argument('--end', default='', type=str, help='end date [YYYY-MM-DD]')
+parser.add_argument('--report', action='store_true', help='generate PDF report')
+args = parser.parse_args()
+
+timeframe = args.timeframe
+start_date = args.start
+end_date = args.end
+now = datetime.now()
+if start_date == '':
+    if timeframe == 'MTD':
+        start_date = datetime(now.year, now.month, 1)
+    elif timeframe == 'YTD':
+        start_date = datetime(now.year, 1, 1)
+    else:
+        print('[ERROR] Unknown timeframe')
+        exit()
+else:
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+
+if end_date == '':
+    end_date = datetime(now.year, now.month, now.day-1)
+else:
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', None)
+
+print("[INFO] Running report for " + timeframe + " (" + str(start_date) + " - " + str(end_date) + ")")
+
+home = '/Users/caolanraff/Desktop/'
+file = home + 'Portfolio_Tracker.xlsx'
+sheets = pd.ExcelFile(file).sheet_names
+
+
+### calculate pnl
+def calculate_portfolio_pnl(file_path, sheet):
+    df = pd.read_excel(file_path, sheet_name=sheet)
+    if len(df) == 0:
+        print("[WARN] Tab is empty for " + sheet)
+        return
+
+    df['date'] = pd.to_datetime(df['date'])
+    max_date = pd.Timestamp.now() - pd.Timedelta(days=1)
+    grouped = df.groupby('ticker')
+    result_df = pd.DataFrame()
+
+    # Calculate running cumulative quantity and running average entry price per ticker
+    for name, group in grouped:
+        # Create a date range from the earliest to latest date for this ticker
+        date_range = pd.date_range(start=group['date'].min(), end=max_date, freq='D')
+        # Create a new dataframe with the complete date range and the ticker symbol
+        date_df = pd.DataFrame({'date': date_range, 'ticker': name})
+        # Merge the original dataframe with the new dataframe
+        merged_df = pd.merge(date_df, group, on=['date', 'ticker'], how='outer')
+        # Fill missing values with 0
+        merged_df = merged_df.fillna(0)
+        # Calculate running cumulative quantity and running average entry price
+        merged_df['cumulative_quantity'] = merged_df['quantity'].cumsum()
+        merged_df['total_cost'] = merged_df['quantity'] * merged_df['price']
+        merged_df['cumulative_cost'] = merged_df['total_cost'].cumsum()
+        merged_df['average_entry_price'] = merged_df['cumulative_cost'] / merged_df['cumulative_quantity']
+        # Append the resulting dataframe for this ticker to the global dataframe
+        result_df = pd.concat([result_df, merged_df], ignore_index=True)
+
+    # Download the adjusted close price from Yahoo Finance for each ticker and date
+    min_date = result_df['date'].min()
+    min_date = min_date - timedelta(days=7)
+    prices = {}
+    for ticker in result_df['ticker'].unique():
+        ticker_data = yf.Ticker(ticker).history(start=min_date, end=max_date)
+        ticker_data = ticker_data.reindex(pd.date_range(min_date, max_date, freq='D'))
+        ticker_data['Close'] = ticker_data['Close'].interpolate()
+        prices[ticker] = ticker_data['Close']
+
+    # Merge the price data onto the original dataframe
+    result_df['market_price'] = result_df.apply(lambda row: prices[row['ticker']].loc[pd.Timestamp(row['date'])], axis=1)
+    # Calculate the notional value based on the market price
+    result_df['notional_value'] = result_df['cumulative_quantity'] * result_df['market_price']
+    # Calculate PnL
+    result_df['pnl'] = result_df['cumulative_quantity'] * (result_df['market_price'] - result_df['average_entry_price'])
+    # Sort the dataframe by date
+    result_df = result_df.sort_values('date')
+    # Calculate the PNL per date
+    result_df['portfolio_pnl'] = result_df.groupby('date')['pnl'].transform('sum')
+    # Calculate the portfolio value per date
+    result_df['portfolio_cost'] = result_df.groupby('date')['cumulative_cost'].transform('sum')
+    result_df['portfolio_value'] = result_df.groupby('date')['notional_value'].transform('sum')
+    # Calculate the PNL per date as a percentage
+    result_df['pnl_pct_per_date'] = 100 * (result_df['portfolio_value'] - result_df['portfolio_cost']) / result_df['portfolio_cost']
+    result_df = result_df.reset_index(drop=True)
+    return result_df
+
+
+### Calculate PnL
+def calculate_all_portfolio_pnl():
+    print('[INFO] Calculating portfolio PnLs')
+    result_dict = {}
+    for sheet in sheets:
+        res = calculate_portfolio_pnl(file, sheet)
+        if res is None:
+            continue
+        res = res[(res['date'] >= start_date) & (res['date'] <= end_date)]
+        result_dict[sheet] = res
+    return result_dict
+
+
+### Save dataframe to PDF
+def save_dataframe_to_pdf(df, file):
+    fig, ax =plt.subplots(figsize=(12,4))
+    ax.axis('tight')
+    ax.axis('off')
+    table = ax.table(cellText=df.values, colLabels=df.columns, loc='center')
+    for (row, col), cell in table.get_celld().items():
+        if row == 0:
+            cell.set_text_props(fontweight='bold', ha='left')
+        else:
+            cell.set_text_props(ha='left')
+    pp = PdfPages(file)
+    pp.savefig(fig, bbox_inches='tight')
+    pp.close()
+
+
+### Create title page
+def create_title_page(aum):
+    pdf_output = FPDF()
+    pdf_output.add_page()
+    title = 'Prestige Worldwide'
+    subtitle = end_date.strftime('%B %Y') + ' Meeting'
+    aum = 'AUM: ' + aum
+    pdf_output.set_font('Arial', 'B', 36)
+    pdf_output.cell(0, 80, title, 0, 1, 'C')
+    pdf_output.set_font('Arial', '', 24)
+    pdf_output.cell(0, 20, subtitle, 0, 1, 'C')
+    pdf_output.set_font('Arial', '', 16)
+    pdf_output.cell(0, 20, aum, 0, 1, 'C')
+    image_file = 'image.png'
+    pdf_output.image(image_file, x=55, y=150, w=100, h=100)
+    pdf_output.output('title.pdf')
+
+
+### Get AUM
+def get_aum(result_dict):
+    print('[INFO] Get AUM')
+    portfolio_val = 0
+
+    for name, df in result_dict.items():
+        res = df.loc[df['date'] == end_date].iloc[0]
+        portfolio_val += res['portfolio_value']
+
+    aum = '$' + '{:,.0f}'.format(portfolio_val)
+    return aum
+
+
+### Get summary info
+def get_summary(result_dict, save_to_file):
+    print('[INFO] Get summary info')
+    val = []
+
+    for name, df in result_dict.items():
+        som = df.loc[df['date'] == start_date].iloc[0]
+        som = 100 * (som['portfolio_value'] - som['portfolio_cost']) / som['portfolio_cost']
+        eom = df.loc[df['date'] == end_date].iloc[0]
+        eom = 100 * (eom['portfolio_value'] - eom['portfolio_cost']) / eom['portfolio_cost']
+        val.append(eom - som)
+
+    summary = pd.DataFrame({'Investor': list(result_dict.keys()), timeframe:val})
+    summary = summary.sort_values(by=timeframe, ascending=False)
+    summary[timeframe] = summary[timeframe].round(3)
+    summary = summary.reset_index(drop=True)
+    summary.loc[0, 'Notes'] = 'Winner'
+    summary.loc[summary.index[-1], 'Notes'] = 'Cheese Duty'
+    summary['Notes'] = summary['Notes'].fillna('')
+
+    if save_to_file:
+        save_dataframe_to_pdf(summary, "summary.pdf")
+    else:
+        print(summary)
+
+
+### Chart
+def plot_performance_charts(result_dict, save_to_file):
+    print('[INFO] Plotting performance charts')
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
+    handles = []  # handles for the legend
+    labels = []   # labels for the legend
+
+    for name, df in result_dict.items():
+        line1, = ax1.plot(df['date'], df['pnl_pct_per_date'], label=name)
+        if name not in labels:
+            handles.append(line1)
+            labels.append(name)
+        ax1.set_xlabel('Date')
+        ax1.set_ylabel('PnL')
+        ax1.set_title('Overall PnL Change')
+
+        df = df.assign(pnl_change=df['pnl_pct_per_date'].diff().cumsum())
+        line2, = ax2.plot(df['date'], df['pnl_change'], label=name)
+        if name not in labels:
+            handles.append(line2)
+            labels.append(name)
+        ax2.set_xlabel('Date')
+        ax2.set_ylabel('PnL')
+        ax2.set_title(timeframe + ' PnL Change')
+
+    for ax in (ax1, ax2):
+        ax.tick_params(axis='x', labelrotation=20)
+    fig.legend(handles, labels)
+
+    if save_to_file:
+        plt.savefig('performance.pdf')
+    else:
+        plt.show()
+
+
+# new trades
+def new_trades(result_dict):
+    print('[INFO] Getting new trades')
+    result_df = pd.DataFrame()
+
+    for key, df in result_dict.items():
+        trades = df.loc[df['quantity'] != 0]
+        tickers = trades['ticker'].to_list()
+        tickers = ', '.join(tickers)
+        result_df = result_df.append({'Investor': key, 'Trades': tickers}, ignore_index=True)
+
+    save_dataframe_to_pdf(result_df, "new_trades.pdf")
+
+
+### best and worst
+# @TODO: best and worst with timeframe
+def best_and_worst(result_dict):
+    print('[INFO] Getting best and worst ETFs')
+    result_df = pd.DataFrame()
+
+    for key, df in result_dict.items():
+        df = df.loc[df['date'] == end_date].copy()
+        total_notional = df['notional_value'].sum()
+        df['pnl_val'] = df['pnl'] / total_notional * 100
+        df['pnl_pct'] = df['pnl'] / df['notional_value'] * 100
+
+        best_portfolio_contribution = df.loc[df['pnl_val'].idxmax(), 'ticker']
+        worst_portfolio_contribution = df.loc[df['pnl_val'].idxmin(), 'ticker']
+        best_pnl_pct = df.loc[df['pnl_pct'].idxmax(), 'ticker']
+        worst_pnl_pct = df.loc[df['pnl_pct'].idxmin(), 'ticker']
+
+        result_df = result_df.append({'Investor': key,
+                                      'Best Portfolio Contributer': best_portfolio_contribution,
+                                      'Worst Portfolio Contributer': worst_portfolio_contribution,
+                                      'Best PnL %': best_pnl_pct,
+                                      'Worst PnL %': worst_pnl_pct
+                                      }, ignore_index=True)
+
+    save_dataframe_to_pdf(result_df, "best_and_worst.pdf")
+
+
+### pie chart
+def plot_pie_charts(result_dict):
+    print('[INFO] Plotting ETF weightings')
+    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(15, 10))
+    plt.subplots_adjust(wspace=0.3, hspace=0.5)
+
+    for i, (key, df) in enumerate(result_dict.items()):
+        row = i // 3
+        col = i % 3
+        df = df.loc[df['date'] == end_date]
+        axs[row, col].pie(df['notional_value'], labels=df['ticker'], autopct='%1.1f%%', radius=1.2)
+        axs[row, col].set_title(key, y=1.1, fontdict={'fontsize': 14, 'fontweight': 'bold'})
+
+    plt.suptitle('ETF Weightings')
+    plt.savefig('weightings.pdf')
+
+
+# Get metrics
+def get_metrics(result_dict):
+    print('[INFO] Getting metrics')
+    result_df = pd.DataFrame()
+
+    for key, df in result_dict.items():
+        tickers = list(df['ticker'].unique())
+        for ticker in tickers:
+            res = si.get_quote_table(ticker)
+            keys = ['Beta (5Y Monthly)', 'Expense Ratio (net)', 'PE Ratio (TTM)', 'YTD Daily Total Return', 'Yield']
+            my_dict = {k: v for k, v in res.items() if k in keys}
+            df = pd.DataFrame([my_dict])
+            df['Investor'] = key
+            df['Ticker'] = ticker
+            df = df[['Investor', 'Ticker'] + list(df.columns.difference(['Investor', 'Ticker']))]
+            result_df = result_df.append(df, ignore_index=True)
+
+    save_dataframe_to_pdf(result_df, "metrics.pdf")
+
+
+### Merge pdf files
+def merge_pdfs(file_list, output_file):
+    print('[INFO] Merging files')
+    pdf_output = pdfrw.PdfWriter()
+    for file_name in file_list:
+        pdf_input = pdfrw.PdfReader(file_name)
+        for page in pdf_input.pages:
+            pdf_output.addpage(page)
+        os.remove(file_name)
+    pdf_output.write(output_file)
+
+
+### main
+def comp():
+    res_dict = calculate_all_portfolio_pnl()
+    aum = get_aum(res_dict)
+    print('[INFO] Prestige Worldwide AUM: ' + aum)
+    get_summary(res_dict, False)
+    plot_performance_charts(res_dict, False)
+
+
+def report():
+    res_dict = calculate_all_portfolio_pnl()
+    aum = get_aum(res_dict)
+    create_title_page(aum)
+    get_summary(res_dict, True)
+    plot_performance_charts(res_dict, True)
+    new_trades(res_dict)
+    best_and_worst(res_dict)
+    plot_pie_charts(res_dict)
+    get_metrics(res_dict)
+    merge_pdfs(['title.pdf', 'summary.pdf', 'performance.pdf', 'new_trades.pdf', 'best_and_worst.pdf', 'weightings.pdf', 'metrics.pdf'],
+               home + 'prestige_worldwide.pdf')
+
+
+if __name__ == "__main__":
+    if args.report:
+        report()
+    else:
+        comp()
